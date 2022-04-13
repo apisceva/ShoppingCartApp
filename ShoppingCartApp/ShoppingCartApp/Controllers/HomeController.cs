196909eb2d6d88d314e0a5e91249f1dd40ef302a
apisceva <aljona.gromova@gmail.com>
2022-04-13 05:18:50 -0400

    home controller

---
+++ ShoppingCartApp/ShoppingCartApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingCartApp.Models;
+using ShoppingCartApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCartApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                AllProducts = _productRepository.AllProducts
+            };
+
+            return View(homeViewModel);
+        }
+    }
+}

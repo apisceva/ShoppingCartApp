5f7ef6834fc9baeb1593b0d9f78d5d063d3c3305
apisceva <aljona.gromova@gmail.com>
2022-04-13 05:27:27 -0400

    new migration ShoppingCartAdded

--- ShoppingCartApp/ShoppingCartApp/Models/AppDbContext.cs
+++ ShoppingCartApp/ShoppingCartApp/Models/AppDbContext.cs
@@ -14,6 +14,7 @@ namespace ShoppingCartApp.Models
         }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {

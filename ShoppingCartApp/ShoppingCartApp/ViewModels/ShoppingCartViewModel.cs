32e48070f4b8389f49a68cec25192982f2d083f6
apisceva <aljona.gromova@gmail.com>
2022-04-13 05:48:54 -0400

    ShoppingCartViewModel added

---
+++ ShoppingCartApp/ShoppingCartApp/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCartApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCartApp.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
